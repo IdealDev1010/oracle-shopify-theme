7f84e1131f1d4a7beb124febec46a486fd44c078
IdealDev1010 <idealdev1010@gmail.com>
2024-01-15 19:07:45 -0600

    update megamenu

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -24,6 +24,13 @@ class DetailsDisclosure extends HTMLElement {
     }
   }
 
+  open() {
+    if (!this.animations) this.animations = this.content.getAnimations();
+    if (this.mainDetailsToggle.hasAttribute('open')) {
+      this.animations.forEach((animation) => animation.play());
+    }
+  }
+
   close() {
     this.mainDetailsToggle.removeAttribute('open');
     this.mainDetailsToggle.querySelector('summary').setAttribute('aria-expanded', false);
@@ -36,6 +43,16 @@ class HeaderMenu extends DetailsDisclosure {
   constructor() {
     super();
     this.header = document.querySelector('.header-wrapper');
+    this.megaMenu = this.querySelector('.mega-menu__content');
+    this.link = this.querySelector('.list-menu__item');
+    this.megaMenu.addEventListener('mouseleave', this.onClose.bind(this));
+    this.link.addEventListener('mouseover', this.onFoucsIn.bind(this));
+  }
+
+  onFoucsIn() {
+    setTimeout(() => {
+      if (!this.contains(document.activeElement)) this.open();
+    });
   }
 
   onToggle() {
@@ -48,6 +65,37 @@ class HeaderMenu extends DetailsDisclosure {
       `${Math.floor(this.header.getBoundingClientRect().bottom)}px`
     );
   }
+  onClose() {
+    this.mainDetailsToggle.open = false;
+    this.header.preventHide = false;
+    
+    if (document.documentElement.style.getPropertyValue('--header-bottom-position-desktop') !== '') return;
+    document.documentElement.style.setProperty(
+      '--header-bottom-position-desktop',
+      `${Math.floor(this.header.getBoundingClientRect().bottom)}px`
+    );
+  }
+
+  open() {
+    if (!this.header) return;
+    this.mainDetailsToggle.open = true;
+    this.header.preventHide = true;
+
+    if (document.documentElement.style.getPropertyValue('--header-bottom-position-desktop') !== '') return;
+    document.documentElement.style.setProperty(
+      '--header-bottom-position-desktop',
+      `${Math.floor(this.header.getBoundingClientRect().bottom)}px`
+    );
+  }
 }
 
 customElements.define('header-menu', HeaderMenu);
+
+document.querySelectorAll('.list-menu__item').forEach(item => {
+  item.addEventListener('mouseover', function(){
+    document.querySelectorAll('header-menu').forEach(headerMenu => {
+     headerMenu.querySelector('details').removeAttribute('open');
+     headerMenu.querySelector('summary').setAttribute('aria-expanded', false);
+    });
+  });
+})
