0fd25f80e8150c5a0fc5447a2d4ffba7ed00e11d
IdealDev1010 <idealdev1010@gmail.com>
2024-01-29 10:47:55 -0600

    update text and icon arrow

--- assets/facets.js
+++ assets/facets.js
@@ -323,3 +323,35 @@ class FacetRemove extends HTMLElement {
 }
 
 customElements.define('facet-remove', FacetRemove);
+
+
+function searchFilter() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('filterSearchInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("ProductGridContainer");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+    if (txtValue == "" ) {
+      li[i].style.display = "block";
+    }
+  }
+}
+
+
+var searchBox = document.getElementById ('filterSearchInput');
+searchBox.addEventListener('input', function() {
+  if (searchBox.value === '') {
+    location.reload();
+  };
+});
